50f6e86057e8bfdde45b43f83c8dddd5eecf0fd0
Redblood639 <albinorabbithunter@gmail.com>
2022-07-21 08:47:08 +0900

    fix : canvase text

--- app/src/components/Canvas.tsx
+++ app/src/components/Canvas.tsx
@@ -1,14 +1,22 @@
 import { FC } from "react";
 import { SendMessage } from "../components/SendMessage";
 
-
 export const Canvas: FC = (props) => {
+  const onWriteMessage = (e: any) => {
+    var canvas: any = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText(e.target.value, canvas.width / 2, canvas.height / 2);
+  };
+
   return (
     <div className="text-center flex">
       <div>
         <canvas
           className="canvas bg-white"
-          id="canvas"
+          id="myCanvas"
           width="640"
           height="640"
         />
@@ -21,12 +29,13 @@ export const Canvas: FC = (props) => {
           id="address"
           placeholder="SOL Address..."
         />
-        <p>Your Message</p>
+        <p>Your Message1</p>
         <input
           className="text-black p-1 m-2"
           type="text"
           id="message"
           placeholder="Your Message..."
+          onChange={(e) => onWriteMessage(e)}
         />
         <SendMessage />
       </div>
--- app/src/components/SendMessage.tsx
+++ app/src/components/SendMessage.tsx
@@ -1,52 +1,62 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction, TransactionSignature } from '@solana/web3.js';
-import { FC, useCallback } from 'react';
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Keypair,
+  SystemProgram,
+  Transaction,
+  TransactionSignature,
+} from "@solana/web3.js";
+import { FC, useCallback } from "react";
 import { notify } from "../utils/notifications";
 
 export const SendMessage: FC = () => {
-    const { connection } = useConnection();
-    const { publicKey } = useWallet();
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
 
-    const onClick = useCallback(async () => {
-        if (!publicKey) {
-            notify({ type: 'error', message: `Wallet not connected!` });
-            console.log('error', `Send Message: Wallet not connected!`);
-            return;
-        }
+  const onClick = useCallback(async () => {
+    if (!publicKey) {
+      notify({ type: "error", message: `Wallet not connected!` });
+      console.log("error", `Send Message: Wallet not connected!`);
+      return;
+    }
 
-        let signature: TransactionSignature = '';
-        try {
-            const transaction = new Transaction().add(
-                SystemProgram.transfer({
-                    fromPubkey: publicKey,
-                    toPubkey: Keypair.generate().publicKey,
-                    lamports: 1,
-                })
-            );
+    let signature: TransactionSignature = "";
+    try {
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: Keypair.generate().publicKey,
+          lamports: 1,
+        })
+      );
 
+      await connection.confirmTransaction(signature, "confirmed");
+      notify({
+        type: "success",
+        message: "Transaction successful!",
+        txid: signature,
+      });
+    } catch (error: any) {
+      notify({
+        type: "error",
+        message: `Transaction failed!`,
+        description: error?.message,
+        txid: signature,
+      });
+      console.log("error", `Transaction failed! ${error?.message}`, signature);
+      return;
+    }
+  }, [publicKey, notify, connection]);
 
-            await connection.confirmTransaction(signature, 'confirmed');
-            notify({ type: 'success', message: 'Transaction successful!', txid: signature });
-        } catch (error: any) {
-            notify({ type: 'error', message: `Transaction failed!`, description: error?.message, txid: signature });
-            console.log('error', `Transaction failed! ${error?.message}`, signature);
-            return;
-        }
-    }, [publicKey, notify, connection]);
-
-    return (
-        <div>
-            <button
-                className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-                onClick={onClick} disabled={!publicKey}
-            >
-                <div className="hidden group-disabled:block ">
-                    Wallet not connected
-                </div>
-                <span className="block group-disabled:hidden" > 
-                    Send Message 
-                </span>
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+        onClick={onClick}
+        disabled={!publicKey}
+      >
+        <div className="hidden group-disabled:block ">Wallet not connected</div>
+        <span className="block group-disabled:hidden">Send Message</span>
+      </button>
+    </div>
+  );
 };
