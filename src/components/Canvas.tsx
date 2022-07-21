91d0e62f15c364c964548acae33859a78dd803d1
Redblood639 <albinorabbithunter@gmail.com>
2022-07-21 10:52:59 +0900

    feat : add the image

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,20 +1,70 @@
-import { FC } from "react";
+import React, { useEffect } from "react";
 import { SendMessage } from "../components/SendMessage";
 
-export const Canvas: FC = (props) => {
+export const Canvas: React.FC = (props) => {
+  const canvas = React.useRef<any>();
+
+  const printAt = (context, text, x, y, lineHeight = 30, fitWidth) => {
+    fitWidth = fitWidth || 0;
+    if (fitWidth <= 0) {
+      context.fillText(text, x, y);
+      return;
+    }
+    for (var idx = 1; idx <= text.length; idx++) {
+      var str = text.substr(0, idx);
+      if (context.measureText(str).width > fitWidth) {
+        context.fillText(text.substr(0, idx - 1), x, y);
+        printAt(
+          context,
+          text.substr(idx - 1),
+          x,
+          y + lineHeight,
+          lineHeight,
+          fitWidth
+        );
+        return;
+      }
+    }
+    context.fillText(text, x, y);
+  };
+
+  const set_Image = () => {
+    var ctx = canvas.current.getContext("2d");
+    let base_image = new Image();
+    base_image.src = "nft.png";
+    ctx.drawImage(
+      base_image,
+      canvas.current.width / 2,
+      canvas.current.height / 2
+    );
+  };
+
   const onWriteMessage = (e: any) => {
-    var canvas: any = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
+    var ctx = canvas.current.getContext("2d");
     ctx.font = "30px Arial";
     ctx.textAlign = "center";
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillText(e.target.value, canvas.width / 2, canvas.height / 2);
+    //  clear letters
+    ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    // image set
+    if (e.target.value !== "") {
+      set_Image();
+    }
+    //  set letters
+    printAt(
+      ctx,
+      e.target.value,
+      canvas.current.width / 2,
+      canvas.current.height / 2,
+      30,
+      canvas.current.width
+    );
   };
 
   return (
     <div className="text-center flex">
       <div>
         <canvas
+          ref={canvas}
           className="canvas bg-white"
           id="myCanvas"
           width="640"
@@ -29,7 +79,7 @@ export const Canvas: FC = (props) => {
           id="address"
           placeholder="SOL Address..."
         />
-        <p>Your Message1</p>
+        <p>Your Message</p>
         <input
           className="text-black p-1 m-2"
           type="text"
